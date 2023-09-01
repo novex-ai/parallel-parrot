7cf2df1997c4c7cea3ba5ec181558af6dc572348
Brad Ito <phlogisticfugu@gmail.com>
2023-09-01 11:52:43 -0700

    fixing issues with openai_api from testing

--- parallel_parrot/openai_api.py
+++ parallel_parrot/openai_api.py
@@ -1,6 +1,6 @@
 from typing import Optional, Union
 
-from aiohttp import ClientSession
+from aiohttp import ClientSession, ClientTimeout
 from aiohttp_retry import RetryClient, JitterRetry
 from pydantic import BaseModel
 
@@ -16,8 +16,7 @@ ClientSessionType = Union[ClientSession, RetryClient]
 class OpenAIChatCompletionConfig(BaseModel):
     openai_api_key: str
     openai_org_id: Optional[str] = None
-    model: str
-    system_message: Optional[str] = None
+    model: str = "gpt-3.5-turbo"
     temperature: Optional[float] = None
     top_p: Optional[float] = None
     n: Optional[int] = None
@@ -42,7 +41,9 @@ def create_chat_completion_client_session(config: OpenAIChatCompletionConfig, us
     headers = create_openai_http_headers(config)
     client_session = ClientSession(
         headers=headers,
-        timeout=OPENAI_REQUEST_TIMEOUT_SECONDS
+        timeout=ClientTimeout(
+            total=OPENAI_REQUEST_TIMEOUT_SECONDS
+        )
     )
     if not use_retries:
         return client_session
@@ -51,14 +52,26 @@ def create_chat_completion_client_session(config: OpenAIChatCompletionConfig, us
     return retry_client_session
 
 
-async def do_chat_completion(client_session: ClientSessionType, config: OpenAIChatCompletionConfig, prompt: str):
-    payload = create_chat_completion_request_payload(config, prompt)
+async def do_chat_completion(client_session: ClientSessionType, config: OpenAIChatCompletionConfig, prompt: str, system_message: Optional[str] = None):
+    payload = create_chat_completion_request_payload(config, prompt, system_message)
     async with client_session.post(OPENAI_CHAT_COMPLETIONS_URL, json=payload) as resp:
         resp.raise_for_status()
         return await resp.json()
 
 
-def create_chat_completion_request_payload(config: OpenAIChatCompletionConfig, prompt: str):
+def parse_chat_completion_result(result: dict):
+    if result.get('object') != 'chat.completion':
+        raise ValueError(f"Unexpected object type: {result.get('object')}")
+    choices = result.get('choices', [])
+    if len(choices) == 0:
+        return None
+    else:
+        choice = choices[0]
+        message = choice.get('message', {})
+        return message.get('content')
+
+
+def create_chat_completion_request_payload(config: OpenAIChatCompletionConfig, prompt: str, system_message: Optional[str] = None):
     """
     https://platform.openai.com/docs/api-reference/chat/create
     """
@@ -83,10 +96,10 @@ def create_chat_completion_request_payload(config: OpenAIChatCompletionConfig, p
     if config.user is not None:
         payload["user"] = config.user
     messages = []
-    if config.system_message is not None:
+    if system_message:
         messages.append({
             "role": "system",
-            "content": config.system_message
+            "content": system_message
         })
     messages.append({
         "role": "user",
