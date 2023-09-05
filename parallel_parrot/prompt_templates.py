d8b5d88e74b7303c7a106a8f3c77f1df24f2b592
Brad Ito <phlogisticfugu@gmail.com>
2023-09-05 14:25:12 -0700

    iterating on prompt templates

--- parallel_parrot/prompt_templates.py
+++ parallel_parrot/prompt_templates.py
@@ -6,8 +6,8 @@ Come up with a single question which is best answered by the following document:
 
 JSON_QUESTION_AND_ANSWER_FROM_DOCUMENT = """
 Generate question and answer pairs from the following document.
-Encode each pair as a list of JSON object with keys "question" and "answer".
+Output a list of JSON objects with keys "question" and "answer".
 example:
-{"question": "What is the capital of France?", "answer": "Paris"}
+[{"question": "What is the capital of France?", "answer": "Paris"},{"question": "What is the capital of Spain?", "answer": "Madrid"}]
 document: ${input}
 """
---
+++ test_json.py
@@ -0,0 +1,57 @@
+import json
+import os
+import logging
+
+import parallel_parrot as pp
+from parallel_parrot.prompt_templates import JSON_QUESTION_AND_ANSWER_FROM_DOCUMENT
+
+
+config = pp.OpenAIChatCompletionConfig(
+    openai_api_key=os.environ["OPENAI_API_KEY"],
+)
+
+logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == '__main__':
+    (result_dictlist, stats) = pp.sync_run(pp.parrot_openai_chat_completion_dictlist(
+        config=config,
+        input_list=[
+            {"input": """
+            Predict Customer Lifetime Value From Day 1
+Leading e-commerce and subscription brands use Retina AI to predict customer lifetime value and make more profitable decisions.
+"""},
+            {"input": """
+            Power your business with Retina
+Get fast, reliable CLV
+Every deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.
+
+ 
+
+Gain a deeper understanding of your customers
+Access dashboards and reports that tell you which segments, customers, and attributes are high-performing and which are dragging down your business profitability.
+
+ 
+
+Improve customer profitability
+Retina’s unique combination of predictive insights and integrations make it easy to identify and act to acquire and retain more high-value customers.
+
+ 
+
+Generate more accurate forecasts
+Go beyond past customer behavior data and start building forecasts based on accurate predictions of future customer behavior and revenue.
+"""},
+        ],
+        prompt_template=JSON_QUESTION_AND_ANSWER_FROM_DOCUMENT,
+        output_key="output",
+    ))
+    print("-------------------------------")
+    qa_outputs = []
+    for row in result_dictlist:
+        output_str = row["output"]
+        try:
+            data = json.loads(output_str)
+            qa_outputs += data
+        except:
+            pass
+    print(json.dumps(qa_outputs, indent=2))
+
