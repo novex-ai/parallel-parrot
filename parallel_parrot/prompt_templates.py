f52dc074a74f28c876d0290a3dff6f523fa6a860
Brad Ito <phlogisticfugu@gmail.com>
2023-09-05 12:18:29 -0700

    adding some initial prompt templates

---
+++ parallel_parrot/prompt_templates.py
@@ -0,0 +1,12 @@
+
+QUESTION_FROM_DOCUMENT = """
+Come up with a single question which is best answered by the following document: ${input}
+"""
+
+JSON_QUESTION_AND_ANSWER_FROM_DOCUMENT = """
+Generate question and answer pairs from the following document.
+Encode each pair as a list of JSON object with keys "question" and "answer".
+example:
+{"question": "What is the capital of France?", "answer": "Paris"}
+document: ${input}
+"""
