66f6fce8df6e31da8729ec170a26cc0f9d721aa3
Brad Ito <phlogisticfugu@gmail.com>
2023-09-06 16:28:17 -0700

    update prompt templates with latest from testing

--- parallel_parrot/prompt_templates.py
+++ parallel_parrot/prompt_templates.py
@@ -7,7 +7,6 @@ Come up with a single question which is best answered by the following document:
 JSON_QUESTION_AND_ANSWER_FROM_DOCUMENT = """
 Generate question and answer pairs from the following document.
 Output a list of JSON objects with keys "question" and "answer".
-example:
-[{"question": "What is the capital of France?", "answer": "Paris"},{"question": "What is the capital of Spain?", "answer": "Madrid"}]
+Only output questions and answers clearly described in the document.  If there are no questions and answers, output an empty list.
 document: ${input}
 """
