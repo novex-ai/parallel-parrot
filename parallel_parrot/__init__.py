f76e4df9a4e6b0efdf840451e3540c4f7fe3c6e2
Brad Ito <phlogisticfugu@gmail.com>
2023-09-06 09:29:31 -0700

    removing need for noqa in __init__.py

--- parallel_parrot/__init__.py
+++ parallel_parrot/__init__.py
@@ -1,7 +1,15 @@
-from .async_util import sync_run  # NOQA
-from .types import OpenAIChatCompletionConfig  # NOQA
+from .async_util import sync_run
+from .types import OpenAIChatCompletionConfig
 from .openai import (
     parrot_openai_chat_completion_dictlist,
     parrot_openai_chat_completion_pandas,
-)  # NOQA
-from .util import auto_explode_json_dictlist  # NOQA
+)
+from .util import auto_explode_json_dictlist
+
+__all__ = (
+    'sync_run',
+    'OpenAIChatCompletionConfig',
+    'parrot_openai_chat_completion_dictlist',
+    'parrot_openai_chat_completion_pandas',
+    'auto_explode_json_dictlist',
+)
