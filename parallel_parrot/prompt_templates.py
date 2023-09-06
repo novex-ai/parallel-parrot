e4051d5aef8c86a873fc93984109d2338a9e49e3
Brad Ito <phlogisticfugu@gmail.com>
2023-09-06 16:36:44 -0700

    adding more prompts

--- parallel_parrot/prompt_templates.py
+++ parallel_parrot/prompt_templates.py
@@ -10,3 +10,12 @@ Output a list of JSON objects with keys "question" and "answer".
 Only output questions and answers clearly described in the document.  If there are no questions and answers, output an empty list.
 document: ${input}
 """
+JSON_QUESTION_AND_ANSWER_FROM_DOCUMENT_KEY_NAMES = ["question", "answer"]
+
+JSON_TITLE_AND_SUMMARY_FROM_DOCUMENT = """
+Generate a title and summary from the following document.
+Output a list of JSON objects with keys "title" and "summary".
+Only output summaries that are accurate and reasonably comprehensive.  If there are no good titles and summaries, output an empty list.
+document: ${input}
+"""
+JSON_TITLE_AND_SUMMARY_FROM_DOCUMENT_KEY_NAMES = ["title", "summary"]
