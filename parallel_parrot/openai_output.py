6f47bb53057cbc5fd51f60213f0c57583ab1ce10
Brad Ito <phlogisticfugu@gmail.com>
2023-09-06 20:25:46 -0700

    updating poetry

--- parallel_parrot/openai_output.py
+++ parallel_parrot/openai_output.py
@@ -1,6 +1,14 @@
-import io
 import json
-from typing import Optional
+from pathlib import Path
+from string import Template
+import tiktoken
+from typing import Optional, Union
+
+from .util import logger
+
+
+# https://platform.openai.com/docs/guides/fine-tuning/token-limits
+FINE_TUNING_MAX_TOKENS = 4096
 
 
 def write_openai_fine_tuning_jsonl(
@@ -8,15 +16,58 @@ def write_openai_fine_tuning_jsonl(
     prompt_key: str,
     completion_key: str,
     system_message: Optional[str],
-    output_file: io.TextIOWrapper
+    model: str,
+    output_file_prefix: Union[str, Path],
+) -> list[Path]:
+    jsonl_generator = openai_fine_tuning_jsonl_generator(
+        input_dictlist=input_dictlist,
+        prompt_key=prompt_key,
+        completion_key=completion_key,
+        system_message=system_message,
+    )
+    encoding = tiktoken.encoding_for_model(model)
+    output_file_prefix_path = Path(output_file_prefix).resolve()
+    output_file_prefix_path.parent.mkdir(parents=True, exist_ok=True)
+    current_num_tokens = 0
+    file_index = 1
+    current_output_file_path = output_file_prefix_path.with_suffix(f".{file_index:0>6}.jsonl")
+    current_open_filehandle = current_output_file_path.open("w")
+    output_file_paths = []
+    for line in jsonl_generator:
+        num_tokens = len(encoding.encode(line))
+        if num_tokens > FINE_TUNING_MAX_TOKENS:
+            logger.warn(f"skipping line with {num_tokens=} {line=}")
+            continue
+        if current_num_tokens + num_tokens > FINE_TUNING_MAX_TOKENS:
+            current_open_filehandle.close()
+            output_file_paths.append(current_output_file_path)
+            logger.info(f"wrote {current_num_tokens} tokens to {str(current_output_file_path)}")
+            file_index += 1
+            current_output_file_path = output_file_prefix_path.with_suffix(f"_{file_index:0>6}.jsonl")
+            current_open_filehandle = current_output_file_path.open("w")
+            current_num_tokens = 0
+        current_open_filehandle.write(line)
+        current_num_tokens += num_tokens
+    current_open_filehandle.close()
+    output_file_paths.append(current_output_file_path)
+    logger.info(f"wrote {current_num_tokens} tokens to {str(current_output_file_path)}")
+    return [
+        str(output_file_path)
+        for output_file_path in output_file_paths
+    ]
+
+
+def openai_fine_tuning_jsonl_generator(
+    input_dictlist: list[dict],
+    prompt_key: str,
+    completion_key: str,
+    system_message: Optional[str] = None,
 ):
-    if not isinstance(output_file, io.TextIOWrapper):
-        raise ValueError(f"{output_file=} must be a io.TextIOWrapper")
     for input_dict in input_dictlist:
         prompt = input_dict[prompt_key]
         completion = input_dict[completion_key]
         messages = []
-        if system_message is not None:
+        if system_message:
             messages.append({
                 "role": "system",
                 "content": system_message,
@@ -29,6 +80,5 @@ def write_openai_fine_tuning_jsonl(
             "role": "assistant",
             "content": completion,
         })
-        json.dump(messages, output_file)
-        output_file.write("\n")
-
+        line = json.dumps(messages, separators=(",", ":")) + "\n"
+        yield line
---
+++ test_fine_tuning_jsonl.py
@@ -0,0 +1,118 @@
+import json
+import logging
+
+import parallel_parrot as pp
+from parallel_parrot.async_util import sync_run
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+input_dictlist = [
+    {
+      "input": "Power your business with Retina\nGet fast, reliable Customer Lifetime Value (CLV)\nEvery deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.",
+      "question": "What can Retina help businesses with?",
+      "answer": "Retina can help businesses with powering their business and getting fast, reliable Customer Lifetime Value (CLV)."
+    },
+    {
+      "input": "Power your business with Retina\nGet fast, reliable Customer Lifetime Value (CLV)\nEvery deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.",
+      "question": "What is delivered related to CLV?",
+      "answer": "Every deliverable related to CLV (scores, analytics, backtest & data explorer) is delivered within hours."
+    },
+    {
+      "input": "Power your business with Retina\nGet fast, reliable Customer Lifetime Value (CLV)\nEvery deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.",
+      "question": "How long does it take to deliver CLV?",
+      "answer": "CLV is delivered within hours, not months or years."
+    },
+    {
+      "input": "Power your business with Retina\nGet fast, reliable Customer Lifetime Value (CLV)\nEvery deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.",
+      "question": "How much does it cost to deliver CLV?",
+      "answer": "The cost of delivering CLV is a fraction of the cost of traditional methods."
+    },
+    {
+      "input": "Power your business with Retina\nGet fast, reliable Customer Lifetime Value (CLV)\nEvery deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.",
+      "question": "What can Retina help with?",
+      "answer": "Retina can help with Customer Lifetime Value (CLV)"
+    },
+    {
+      "input": "Power your business with Retina\nGet fast, reliable Customer Lifetime Value (CLV)\nEvery deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.",
+      "question": "How fast is the delivery of CLV-related deliverables?",
+      "answer": "CLV-related deliverables are delivered within hours"
+    },
+    {
+      "input": "Power your business with Retina\nGet fast, reliable Customer Lifetime Value (CLV)\nEvery deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.",
+      "question": "What is the accuracy of CLV?",
+      "answer": "CLV is 90%+ accurate"
+    },
+    {
+      "input": "Power your business with Retina\nGet fast, reliable Customer Lifetime Value (CLV)\nEvery deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.",
+      "question": "What is the cost of CLV-related deliverables?",
+      "answer": "CLV-related deliverables cost a fraction of the cost"
+    },
+    {
+      "input": "Power your business with Retina\nGet fast, reliable Customer Lifetime Value (CLV)\nEvery deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.",
+      "question": "What can Retina help with?",
+      "answer": "Retina can help with fast and reliable Customer Lifetime Value (CLV) calculations."
+    },
+    {
+      "input": "Power your business with Retina\nGet fast, reliable Customer Lifetime Value (CLV)\nEvery deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.",
+      "question": "How fast can Retina deliver CLV-related deliverables?",
+      "answer": "Retina can deliver CLV-related deliverables within hours."
+    },
+    {
+      "input": "Power your business with Retina\nGet fast, reliable Customer Lifetime Value (CLV)\nEvery deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.",
+      "question": "How accurate are the CLV scores delivered by Retina?",
+      "answer": "The CLV scores delivered by Retina are 90%+ accurate."
+    },
+    {
+      "input": "Power your business with Retina\nGet fast, reliable Customer Lifetime Value (CLV)\nEvery deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.",
+      "question": "What is the advantage of using Retina for CLV calculations?",
+      "answer": "The advantage of using Retina for CLV calculations is that it is much faster and more cost-effective compared to other methods that can take months or years."
+    },
+    {
+      "input": "The Predictive Customer Insights Platform\nRetina is your complete, self-serve solution for predicting, understanding, and acting on future customer behavior. Whether you\u2019re trying to optimize customer acquisition costs or better understand which high-value customers are at risk, Retina has you covered.",
+      "question": "What is Retina?",
+      "answer": "Retina is a complete, self-serve solution for predicting, understanding, and acting on future customer behavior."
+    },
+    {
+      "input": "The Predictive Customer Insights Platform\nRetina is your complete, self-serve solution for predicting, understanding, and acting on future customer behavior. Whether you\u2019re trying to optimize customer acquisition costs or better understand which high-value customers are at risk, Retina has you covered.",
+      "question": "What can Retina help with?",
+      "answer": "Retina can help optimize customer acquisition costs and better understand which high-value customers are at risk."
+    },
+    {
+      "input": "The Predictive Customer Insights Platform\nRetina is your complete, self-serve solution for predicting, understanding, and acting on future customer behavior. Whether you\u2019re trying to optimize customer acquisition costs or better understand which high-value customers are at risk, Retina has you covered.",
+      "question": "What is Retina?",
+      "answer": "Retina is a complete, self-serve solution for predicting, understanding, and acting on future customer behavior."
+    },
+    {
+      "input": "The Predictive Customer Insights Platform\nRetina is your complete, self-serve solution for predicting, understanding, and acting on future customer behavior. Whether you\u2019re trying to optimize customer acquisition costs or better understand which high-value customers are at risk, Retina has you covered.",
+      "question": "What can Retina help with?",
+      "answer": "Retina can help optimize customer acquisition costs and better understand which high-value customers are at risk."
+    },
+    {
+      "input": "The Predictive Customer Insights Platform\nRetina is your complete, self-serve solution for predicting, understanding, and acting on future customer behavior. Whether you\u2019re trying to optimize customer acquisition costs or better understand which high-value customers are at risk, Retina has you covered.",
+      "question": "What is Retina?",
+      "answer": "Retina is a complete, self-serve solution for predicting, understanding, and acting on future customer behavior."
+    },
+    {
+      "input": "The Predictive Customer Insights Platform\nRetina is your complete, self-serve solution for predicting, understanding, and acting on future customer behavior. Whether you\u2019re trying to optimize customer acquisition costs or better understand which high-value customers are at risk, Retina has you covered.",
+      "question": "What can Retina help with?",
+      "answer": "Retina can help optimize customer acquisition costs and better understand high-value customers at risk."
+    }
+  ]
+
+
+async def main():
+    paths = pp.write_openai_fine_tuning_jsonl(
+        input_dictlist=input_dictlist,
+        prompt_key="question",
+        completion_key="answer",
+        system_message="",
+        model="gpt-3.5-turbo-0613",
+        output_file_prefix="/tmp/parallel_parrot/test_fine_tuning_jsonl",
+    )
+    print(json.dumps(paths, indent=2, default=str))
+
+
+if __name__ == "__main__":
+    sync_run(main())
