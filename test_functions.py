eb754551f5d3bf490c04fa624de4a8e2ad917194
Brad Ito <phlogisticfugu@gmail.com>
2023-09-07 20:46:25 -0700

    move to more generic documents

--- test_functions.py
+++ test_functions.py
@@ -8,7 +8,7 @@ from parallel_parrot.async_util import sync_run
 config = pp.OpenAIChatCompletionConfig(
     openai_api_key=os.environ["OPENAI_API_KEY"],
     model="gpt-3.5-turbo-0613",
-    n=3,
+    n=2,
 )
 
 logging.basicConfig(level=logging.DEBUG)
@@ -20,16 +20,13 @@ async def main():
         input_list=[
             {
                 "input": """
-Power your business with Retina
-Get fast, reliable Customer Lifetime Value (CLV)
-Every deliverable related to 90%+ accurate CLV (scores, analytics, backtest & data explorer) is delivered within hours vs. months or years and at a fraction of the cost.
+George Washington (February 22, 1732 - December 14, 1799) was an American military officer, statesman, and Founding Father who served as the first president of the United States from 1789 to 1797. Appointed by the Second Continental Congress as commander of the Continental Army in June 1775, Washington led Patriot forces to victory in the American Revolutionary War and then served as president of the Constitutional Convention in 1787, which drafted and ratified the Constitution of the United States and established the American federal government. Washington has thus been called the "Father of his Country".
                 """.strip()
             },
             {
                 "input": """
-The Predictive Customer Insights Platform
-Retina is your complete, self-serve solution for predicting, understanding, and acting on future customer behavior. Whether you’re trying to optimize customer acquisition costs or better understand which high-value customers are at risk, Retina has you covered.
-                """.strip()
+John Adams (October 30, 1735 - July 4, 1826) was an American statesman, attorney, diplomat, writer, and Founding Father who served as the second president of the United States from 1797 to 1801. Before his presidency, he was a leader of the American Revolution that achieved independence from Great Britain. During the latter part of the Revolutionary War and in the early years of the new nation, he served the U.S. government as a senior diplomat in Europe. Adams was the first person to hold the office of vice president of the United States, serving from 1789 to 1797. He was a dedicated diarist and regularly corresponded with important contemporaries, including his wife and adviser Abigail Adams and his friend and political rival Thomas Jefferson.
+               """.strip()
             }
         ],
         prompt_template="""
