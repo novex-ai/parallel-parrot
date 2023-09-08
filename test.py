1efa022c9f2422a47471be713ac7f8aeb27a4ec8
Brad Ito <phlogisticfugu@gmail.com>
2023-09-07 20:32:17 -0700

    adding unit test for pandas

--- test.py
+++ test.py
@@ -1,25 +1,37 @@
 import os
 import logging
 
+import pandas as pd
 import parallel_parrot as pp
 
 
 config = pp.OpenAIChatCompletionConfig(
     openai_api_key=os.environ["OPENAI_API_KEY"],
+    model="gpt-3.5-turbo-0613",
 )
 
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
-    output = pp.sync_run(pp.parrot_openai_chat_completion_dictlist(
-        config=config,
-        input_list=[
-            {"input": "what is 1+1?"},
-            {"input": "what is 2+2?"},
-            {"input": "what is 3+3?"},
+    input_df = pd.DataFrame({
+        "input": [
+            "this is a super duper product that will change the world",
+            "do not buy this",
         ],
-        prompt_template="Q: ${input}\nA:",
-        output_key="output",
-        system_message="you are a super-precise calculator that only returns answers"
+        "source": [
+            "amazon",
+            "shopify",
+        ]
+    }, index=[100, 101])
+    (output_df, usage_stats_sum) = pp.sync_run(pp.parrot_openai_chat_completion_pandas(
+        config=config,
+        input_df=input_df,
+        prompt_template="""
+What is the sentiment of this product review?
+POSITIVE or NEGATIVE?
+product review: ${input}
+sentiment:""",
+        output_key="sentiment",
     ))
-    print(repr(output))
+    print(repr(output_df['sentiment']))
+    print(repr(usage_stats_sum))
--- tests/test_openai.py
+++ tests/test_openai.py
@@ -3,6 +3,11 @@ import pytest
 
 import parallel_parrot as pp
 
+try:
+    import pandas as pd
+except ImportError:
+    pd = None
+
 
 @pytest.fixture
 def mock_aioresponse():
@@ -88,4 +93,78 @@ def test_parrot_openai_chat_completion_dictlist(
         "completion_tokens": 2,
         "prompt_tokens": 74,
         "total_tokens": 76,
+    }
+
+
+@pytest.mark.skipif(pd is None, reason="requires pandas")
+def test_parrot_openai_chat_completion_pandas(
+    mock_aioresponse, openai_chat_completion_config
+):
+    mock_aioresponse.post(
+        "https://api.openai.com/v1/chat/completions",
+        headers={
+            "x-ratelimit-limit-requests": "3500",
+        },
+        payload={
+            "id": "chatcmpl-7wMJooPRFYJLk1PKLQLAEt2kPmAHF",
+            "object": "chat.completion",
+            "created": 1694143172,
+            "model": "gpt-3.5-turbo-0613",
+            "choices": [
+                {
+                    "index": 0,
+                    "message": {"role": "assistant", "content": "POSITIVE"},
+                    "finish_reason": "stop",
+                }
+            ],
+            "usage": {"prompt_tokens": 41, "completion_tokens": 2, "total_tokens": 43},
+        },
+    )
+    mock_aioresponse.post(
+        "https://api.openai.com/v1/chat/completions",
+        payload={
+            "id": "chatcmpl-7wMJoagRJIyE35cVIMTzEtHCnlqCa",
+            "object": "chat.completion",
+            "created": 1694143172,
+            "model": "gpt-3.5-turbo-0613",
+            "choices": [
+                {
+                    "index": 0,
+                    "message": {"role": "assistant", "content": "NEGATIVE"},
+                    "finish_reason": "stop",
+                }
+            ],
+            "usage": {"prompt_tokens": 33, "completion_tokens": 2, "total_tokens": 35},
+        },
+    )
+    input_df = pd.DataFrame(
+        {
+            "input": [
+                "this is a super duper product that will change the world",
+                "do not buy this",
+            ],
+            "source": [
+                "amazon",
+                "shopify",
+            ],
+        },
+        index=[100, 101],
+    )
+    (output_df, usage_stats_sum) = pp.sync_run(
+        pp.parrot_openai_chat_completion_pandas(
+            config=openai_chat_completion_config,
+            input_df=input_df,
+            prompt_template="""
+What is the sentiment of this product review?
+POSITIVE or NEGATIVE?
+product review: ${input}
+sentiment:""",
+            output_key="sentiment",
+        )
+    )
+    assert output_df["sentiment"].tolist() == ["POSITIVE", "NEGATIVE"]
+    assert usage_stats_sum == {
+        "completion_tokens": 4,
+        "prompt_tokens": 74,
+        "total_tokens": 78,
     }
