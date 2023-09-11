0c705ddd07fd2aa6933ac2cd3949cb20ebcf536e
Brad Ito <phlogisticfugu@gmail.com>
2023-09-11 15:58:12 -0700

    adding another common prompt template

--- parallel_parrot/prompt_templates.py
+++ parallel_parrot/prompt_templates.py
@@ -4,6 +4,10 @@ QUESTION_FROM_DOCUMENT = """
 Come up with a single question which is best answered by the following document: ${input}
 """
 
+SUMMARY_FROM_DOCUMENT = """
+Generate a concise summary of the following document: ${input}
+"""
+
 JSON_QUESTION_AND_ANSWER_FROM_DOCUMENT = """
 Generate question and answer pairs from the following document.
 Output a list of JSON objects with keys "question" and "answer".
