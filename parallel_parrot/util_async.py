d05928e7998373c6c7d4cc39e65e0f4574fd0dab
Brad Ito <phlogisticfugu@gmail.com>
2023-09-15 08:15:46 -0700

    refactor run_async to separate concerns

--- parallel_parrot/util_async.py
+++ parallel_parrot/util_async.py
@@ -14,28 +14,19 @@ from .util import logger
 
 def run_async(coro: Coroutine) -> Any:
     """
-    Run an async coroutine synchronously.
+    Safely run an async coroutine synchronously (without await).
+    Do this irregardless of the current execution environment (IPython-based notebook, script, etc)
     Try to use uvloop for faster i/o if possible
     """
     event_loop = _safe_get_running_loop()
-    if uvloop_installed and event_loop is None:
-        pre_existing_policy = asyncio.get_event_loop_policy()
-        logger.debug("using uvloop for faster asyncio")
-        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    if event_loop and event_loop.is_running():
+        logger.info("event loop alreadyr running.  Using thread to run coroutine")
+        thread = _AsyncRunnerThread(coro)
+        thread.start()
+        thread.join()
+        return thread.result
     else:
-        pre_existing_policy = None
-    try:
-        if event_loop and event_loop.is_running():
-            logger.info("event loop alreadyr running.  Using thread to run coroutine")
-            thread = _AsyncRunnerThread(coro)
-            thread.start()
-            thread.join()
-            return thread.result
-        else:
-            return asyncio.run(coro)
-    finally:
-        if pre_existing_policy:
-            asyncio.set_event_loop_policy(pre_existing_policy)
+        return _fast_run_async(coro)
 
 
 class _AsyncRunnerThread(threading.Thread):
@@ -49,7 +40,21 @@ class _AsyncRunnerThread(threading.Thread):
         super().__init__()
 
     def run(self):
-        self.result = asyncio.run(self.coro)
+        self.result = _fast_run_async(self.coro)
+
+
+def _fast_run_async(coro: Coroutine) -> Any:
+    if uvloop_installed:
+        pre_existing_policy = asyncio.get_event_loop_policy()
+        logger.debug("using uvloop for faster asyncio")
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    else:
+        pre_existing_policy = None
+    try:
+        return asyncio.run(coro)
+    finally:
+        if pre_existing_policy:
+            asyncio.set_event_loop_policy(pre_existing_policy)
 
 
 def _safe_get_running_loop():
